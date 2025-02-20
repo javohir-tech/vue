955a8ba69e9df5a0aa7f025302a187d26bda635f
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-02-20 21:12:27 +0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+    export default{
+        data(){
+            return {
+                counter : 0
+            }
+        },
+        methods:{
+            onIncrement(){
+                this.counter += 1;
+            },
+            onDecrement (){
+                if(this.counter > 0){
+                    this.counter -= 1;
+                }
+            }
+        }
+    }
 </script>
 
 <template>
- salom
+    <div>
+        <h1>Counter : {{ counter }}</h1>
+        <button @click="onIncrement">add</button>
+        <button @click="onDecrement">delete</button>
+    </div>
 </template>
 
+<style>
+
+</style>
